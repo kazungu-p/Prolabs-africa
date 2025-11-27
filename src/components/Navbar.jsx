825f6977e7142108d702b01eb2f791a55015760d
kazungu-p <patrickkazungu@icloud.com>
2025-11-27 15:19:44 +0300

    Add navbar and styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../assets/styles/navbar.css"
+
+function Navbar() {
+
+    const [menuOpen, setMenuOpen] = useState("false");
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen);
+    }
+
+    return (
+        <header>
+            <nav>
+                <div className="logo">
+                    <img src="images/logo.jpg" alt="prolabs-africa logo" />
+                </div>
+                <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Portfolio</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="nav-btn">
+                    <button>Request a Quote</button>
+                </div>
+                <div className="hamburger">
+                    {menuOpen ? "\u2715" : "\u2630"}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
